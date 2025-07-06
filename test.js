ec2f286cbd5ea38648cebe0967749d8c8cdeb498
DevSecOpsEng Jr <113997331+Heilkern@users.noreply.github.com>
2025-07-07 08:22:33 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,6 @@
 console.log("hello, aws classroom!")
+console.log("hello, world!")
+console.log("hello, lambda!")
 const now = new Date();
 console.log(now.toString());
 console.log(now.toLocaleTimeString())
